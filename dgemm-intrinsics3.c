21ff49a9537f46e4406774f533f66f4856a07cc1
Qiaochu Yuan <qyuan@hopper01.(none)>
2015-02-12 19:02:10 -0800

    final copy

--- dgemm-intrinsics3.c
+++ dgemm-intrinsics3.c
@@ -7,7 +7,7 @@
 const char* dgemm_desc = "Blocked dgemm with intrinsics, register blocking, outer product version";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 4
+#define BLOCK_SIZE 40
 #endif
 
 #if !defined(min)
@@ -16,56 +16,56 @@ const char* dgemm_desc = "Blocked dgemm with intrinsics, register blocking, oute
 
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. 
+ * where C is BLOCK_SIZE-by-BLOCK_SIZE, A is M-by-K, and B is K-by-N. 
  * Assume that block dimensions are divisible by 4 */
-static void do_block (int M, int N, int K, double* A, double* B, double* C)
+static void do_block (double* A, double* B, double* C)
 {
   __m128d c1,c2,c3,c4,c5,c6,c7,c8,a1,a2,b; 
   /* For each row i of A */
-  for (int j = 0; j < N; j+=4)
+  for (int j = 0; j < BLOCK_SIZE; j+=4)
   {
     /* For each column j of B */ 
-    for (int i = 0; i < M; i+=4) 
+    for (int i = 0; i < BLOCK_SIZE; i+=4) 
     {
       /* Compute C(i,j) */
-      c1 = _mm_load_pd(C+i+j*M);
-      c2 = _mm_load_pd(C+i+2+j*M);
-      c3 = _mm_load_pd(C+i+(j+1)*M);
-      c4 = _mm_load_pd(C+i+2+(j+1)*M);
-      c5 = _mm_load_pd(C+i+(j+2)*M);
-      c6 = _mm_load_pd(C+i+2+(j+2)*M);
-      c7 = _mm_load_pd(C+i+(j+3)*M);
-      c8 = _mm_load_pd(C+i+2+(j+3)*M);
+      c1 = _mm_load_pd(C+i+j*BLOCK_SIZE);
+      c2 = _mm_load_pd(C+i+2+j*BLOCK_SIZE);
+      c3 = _mm_load_pd(C+i+(j+1)*BLOCK_SIZE);
+      c4 = _mm_load_pd(C+i+2+(j+1)*BLOCK_SIZE);
+      c5 = _mm_load_pd(C+i+(j+2)*BLOCK_SIZE);
+      c6 = _mm_load_pd(C+i+2+(j+2)*BLOCK_SIZE);
+      c7 = _mm_load_pd(C+i+(j+3)*BLOCK_SIZE);
+      c8 = _mm_load_pd(C+i+2+(j+3)*BLOCK_SIZE);
      
-      for (int k = 0; k < K; ++k)
+      for (int k = 0; k < BLOCK_SIZE; ++k)
       {
-        a1 = _mm_load_pd(A+i+k*M);
-        a2 = _mm_load_pd(A+i+2+k*M);
+        a1 = _mm_load_pd(A+i+k*BLOCK_SIZE);
+        a2 = _mm_load_pd(A+i+2+k*BLOCK_SIZE);
       
-        b = _mm_load1_pd(B+j+k*N);
+        b = _mm_load1_pd(B+j+k*BLOCK_SIZE);
 	c1 = _mm_add_pd(_mm_mul_pd(a1,b),c1);
 	c2 = _mm_add_pd(_mm_mul_pd(a2,b),c2);
 
-        b = _mm_load1_pd(B+j+1+k*N);
+        b = _mm_load1_pd(B+j+1+k*BLOCK_SIZE);
  	c3 = _mm_add_pd(_mm_mul_pd(a1,b),c3);
 	c4 = _mm_add_pd(_mm_mul_pd(a2,b),c4);
 
-        b = _mm_load1_pd(B+j+2+k*N);
+        b = _mm_load1_pd(B+j+2+k*BLOCK_SIZE);
  	c5 = _mm_add_pd(_mm_mul_pd(a1,b),c5);
 	c6 = _mm_add_pd(_mm_mul_pd(a2,b),c6);
 
-        b = _mm_load1_pd(B+j+3+k*N);
+        b = _mm_load1_pd(B+j+3+k*BLOCK_SIZE);
  	c7 = _mm_add_pd(_mm_mul_pd(a1,b),c7);
 	c8 = _mm_add_pd(_mm_mul_pd(a2,b),c8);
      }
-      _mm_store_pd(C+i+j*M,c1);
-      _mm_store_pd(C+i+2+j*M,c2);
-      _mm_store_pd(C+i+(j+1)*M,c3);
-      _mm_store_pd(C+i+2+(j+1)*M,c4);
-      _mm_store_pd(C+i+(j+2)*M,c5);
-      _mm_store_pd(C+i+2+(j+2)*M,c6);
-      _mm_store_pd(C+i+(j+3)*M,c7);
-      _mm_store_pd(C+i+2+(j+3)*M,c8);
+      _mm_store_pd(C+i+j*BLOCK_SIZE,c1);
+      _mm_store_pd(C+i+2+j*BLOCK_SIZE,c2);
+      _mm_store_pd(C+i+(j+1)*BLOCK_SIZE,c3);
+      _mm_store_pd(C+i+2+(j+1)*BLOCK_SIZE,c4);
+      _mm_store_pd(C+i+(j+2)*BLOCK_SIZE,c5);
+      _mm_store_pd(C+i+2+(j+2)*BLOCK_SIZE,c6);
+      _mm_store_pd(C+i+(j+3)*BLOCK_SIZE,c7);
+      _mm_store_pd(C+i+2+(j+3)*BLOCK_SIZE,c8);
     }
   }
 
@@ -79,8 +79,8 @@ void pad_and_pack_col(int lda, double *new, double *old)
   int blockrowind, blockcolind, rowind, colind, cwidth, rwidth;
   int newlda = lda;
 
-  if (lda % 4 != 0)
-    newlda = (lda/4)*4 + 4; // if odd dimensioned matrix, pad
+  if (lda % BLOCK_SIZE != 0)
+    newlda = (lda/BLOCK_SIZE)*BLOCK_SIZE + BLOCK_SIZE; // if odd dimensioned matrix, pad
 
   for (int j = 0; j < lda; ++j)
     for (int i = 0; i < lda; ++i)
@@ -102,8 +102,8 @@ void unpad_and_unpack_col(int lda, double *packed, double *unpacked)
   int blockrowind, blockcolind, rowind, colind, cwidth, rwidth;
   int newlda = lda;
 
-  if (lda % 4 != 0)
-    newlda = (lda/4)*4 + 4;
+  if (lda % BLOCK_SIZE != 0)
+    newlda = (lda/BLOCK_SIZE)*BLOCK_SIZE + BLOCK_SIZE;
 
   for (int j = 0; j < lda; ++j)
     for (int i = 0; i < lda; ++i)
@@ -124,8 +124,8 @@ void pad_and_pack_row(int lda, double *new, double *old)
   int blockrowind, blockcolind, rowind, colind, cwidth, rwidth;
   int newlda = lda;
 
-  if (lda % 4 != 0)
-    newlda = (lda/4)*4 + 4; // if odd dimensioned matrix, pad
+  if (lda % BLOCK_SIZE != 0)
+    newlda = (lda/BLOCK_SIZE)*BLOCK_SIZE + BLOCK_SIZE; // if odd dimensioned matrix, pad
 
   for (int j = 0; j < lda; ++j)
     for (int i = 0; i < lda; ++i)
@@ -148,8 +148,8 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 {
   double *newA, *newB, *newC;
   int newlda = lda;
-  if (lda % 4 != 0)
-    newlda = (lda/4)*4 + 4;
+  if (lda % BLOCK_SIZE != 0)
+    newlda = (lda/BLOCK_SIZE)*BLOCK_SIZE + BLOCK_SIZE;
   newA = (double*)_mm_malloc(sizeof(double)*newlda*newlda,128);
   newB = (double*)_mm_malloc(sizeof(double)*newlda*newlda,128);
   newC = (double*)_mm_malloc(sizeof(double)*newlda*newlda,128);
@@ -164,13 +164,9 @@ void square_dgemm (int lda, double* A, double* B, double* C)
     for (int i = 0; i < newlda; i += BLOCK_SIZE)
       for (int k = 0; k < newlda; k += BLOCK_SIZE)
       {
-	int M = min (BLOCK_SIZE, newlda-i);
-        int N = min (BLOCK_SIZE, newlda-j);
-	int K = min (BLOCK_SIZE, newlda-k);
-
 	/* Perform individual block dgemm */
-	do_block(M, N, K, newA + k*newlda + i*M, 
-          newB + k*newlda + j*N, newC + j*newlda + i*N);
+	do_block(newA + k*newlda + i*BLOCK_SIZE, 
+          newB + k*newlda + j*BLOCK_SIZE, newC + j*newlda + i*BLOCK_SIZE);
       }
 
   unpad_and_unpack_col(lda,newC,C);
